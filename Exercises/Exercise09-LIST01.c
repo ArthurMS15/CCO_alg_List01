509164a38c683f3daf96ac4617c8ef2849db850a
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-01-26 17:18:37 -0300

    Exercise 9

---
+++ Exercises/Exercise09-LIST01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    float current_wage, adjusted_wage, adjustment;
+    adjustment = 23.75/100;
+    printf("Indicate your current wage:");
+    scanf("%f", &current_wage);
+    adjusted_wage = (current_wage*adjustment) + current_wage;
+    printf("Your adjusted wage is: %.2f\n", adjusted_wage);
+}
