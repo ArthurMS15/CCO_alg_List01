ab3f9bf37dd012c6a26ce55eb5bb0270b6cb49fa
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-01-26 17:00:41 -0300

    Exercise 6

---
+++ Exercises/Exercise06-LIST01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float radius, spherevol;
+    const float PI = 3.14;
+    printf("Specify radius value:");
+    scanf("%f", &radius);
+    spherevol = 4/3*PI*pow(radius,3);
+    printf("Sphere volume is: %.2f\n", spherevol);
+}
