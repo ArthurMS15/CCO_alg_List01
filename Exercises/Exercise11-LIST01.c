66088f1557d5e1c644ff18ee9fdf0d8502b4756d
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-01-26 17:25:43 -0300

    Exercise 11

---
+++ Exercises/Exercise11-LIST01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    float late_payment, installment, tax, nlatedays;
+    printf("Specify installment value:");
+    scanf("%f", &installment);
+    printf("Specify tax value:");
+    scanf("%f", &tax);
+    printf("Specify the number of late days:");
+    scanf("%f", &nlatedays);
+    late_payment = installment+(installment*(tax/100)*nlatedays);
+    printf("Late payment value is: %.2f\n", late_payment);
+}
