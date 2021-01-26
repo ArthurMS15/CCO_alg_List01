7268cd33d7ea39888feb50c82b2e40eb64e61002
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-01-26 16:38:18 -0300

    Exercise 3

---
+++ Exercises/Exercise03-LIST01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float radius, sphere;
+    const float PI = 3.14;
+    printf("Specify the radius value:");
+    scanf("%f", &radius);
+    sphere = 4*PI*pow(radius,2);
+    printf("Superficial area of the sphere is: %.2f\n", sphere);
+}
