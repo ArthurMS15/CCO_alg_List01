22c3c4947a1834802119f9b2272720513ad3944d
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-01-26 21:11:00 -0300

    Exercise 24

---
+++ Exercises/Exercise24-LIST01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+    int n1;
+    printf("Enter the number value (just int)\n");
+    scanf("%d", &n1);
+    if (n1 % 2 == 0) printf("n1 is even\n");
+    else printf("n1 is odd\n");
+    return 0;
+}
