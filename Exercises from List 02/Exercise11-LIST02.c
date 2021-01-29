99adadb3282e2a3382e4212125c07cbfb40c00a6
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-01-29 19:03:43 -0300

    Exercise 11

---
+++ Exercises from List 02/Exercise11-LIST02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int count, sum;
+    count=1;
+    sum=0;
+    while(count<=100){
+        sum=sum+count;
+        count++;
+    }
+    printf("The sum of all first 100 natural numbers is: %d\n", sum);
+}
