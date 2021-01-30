26b943eaa34ed99b1bd4310ed2af8fe0d955a084
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-01-30 17:34:12 -0300

    Exercise 4

--- Exercises from List 04/Exercise04-LIST04.c
+++ Exercises from List 04/Exercise04-LIST04.c
@@ -2,21 +2,32 @@
 #define N 12
 
 int main(){
-    int vectn[N], x1=0-1, y1=0-1;
+    int vectn[N];
     for(int i=0;i<N;i++){
         printf("Type the %d value for the vector:\n", i+1);
         scanf("%d", &vectn[i]);
     }
-    porintf("\nVector n:");
-    int vectx[x1], vecty[y1];
+    printf("\nVector n:");
+    int vectx[N], vecty[N];
+    int x1=0;
+    int y1=0;
     for(int i=0;i<N;i++){
         printf("%d\t", vectn[i]);
         if(vectn[i]%2==0 && vectn[i]%3==0){
-            x1++;
             vectx[x1]=vectn[i];
+            x1++;
         } else if (vectn[i]%2==1 && vectn[i]%5==0){
-            y1++;
             vecty[y1]=vectn[i];
+            y1++;
         }
     }
+    printf("\n");
+    for(int i=0;i<x1;i++){
+        printf("%d\t", vectx[i]);
+    }
+    printf("\n");
+    for(int i=0;i<y1;i++){
+        printf("%d\t", vecty[i]);
+    }
+
 }
