caf763c6f27b2728fb0936b77064a91d7836280a
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-01-30 02:02:57 -0300

    Exercise 1

--- Exercises from List 04/Exercise01-LIST04.c
+++ Exercises from List 04/Exercise01-LIST04.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(){
-    int vect[10];
+    int vect[10], count=0;
     
     for(int i=0;i<10;i++){
         vect[i]=rand()%30;
@@ -15,7 +15,12 @@ int main(){
     for(int i=0;i<10;i++){
         if(vect[i]>20){
             printf("\t%d", vect[i]);
+        } else {
+            count++;
         }
+    }
+    if(count==10){
+        printf("There is no value higher than 20\n");
     }
 
     return 0;
