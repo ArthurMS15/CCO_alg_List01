359fa4346ebb1a1741c1a8023913c47f44fe4879
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-01-29 21:56:54 -0300

    Exercise 17

---
+++ Exercises from List 02/Exercise17-LIST02.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    float net_wage, gross_wage, nhours, count, income_tax, charges, additional, extrahours, total;
+    count=0;
+    while(count<1){
+        printf("Insert your gross wage:\n");
+        scanf("%f", &gross_wage);
+        if(gross_wage==0){
+            count++;
+        } else {
+            printf("Insert the numbers of hours worked:\n");
+            scanf("%f", &nhours);
+            if(800<=gross_wage<=1600){
+                income_tax=gross_wage*0.08;
+                charges=gross_wage*0.05;
+            } else if (gross_wage>1600){
+                income_tax=gross_wage*0.15;
+                charges=gross_wage*0.07;
+            }
+            if(nhours>160){
+                extrahours=nhours-160;
+                additional = ((gross_wage/160)*1.5)*extrahours;
+            }
+            net_wage=gross_wage-income_tax-charges+additional;
+            printf("Net wage: %.2f\n", net_wage);
+            total=total+net_wage;
+        }
+    }
+    printf("All net wages: %.2f\n", total);
+}
