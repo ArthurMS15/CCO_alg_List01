391d390184b5020209c77102b3306690e1fa409b
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-01-29 21:18:30 -0300

    Exercise 16

---
+++ Exercises from List 02/Exercise16-LIST02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float popa=90000000;
+    float popb=140000000;
+    int count;
+    while(popa<=popb){
+        popa=(popa+(popa*0.035));
+        popb=(popb+(popb*0.01));
+        count++;
+    }
+    printf("The population of country A will surpass population of country B in: %d years\n", count);
+    return 0;
+}
