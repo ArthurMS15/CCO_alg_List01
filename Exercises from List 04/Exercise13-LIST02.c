6842180a4c31f6961be4e13ec84f55549dc0128e
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-01-30 21:01:19 -0300

    Exercise 13

---
+++ Exercises from List 04/Exercise13-LIST02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define LINE 7
+#define COLUMN 8
+
+int main(){
+    int array[LINE][COLUMN];
+
+    for(int i=0;i<LINE;i++){
+        for(int j=0;j<COLUMN;j++){
+            array[i][j]=i+j;
+        }
+    }
+    printf("Array:\n");
+    for(int i=0;i<LINE;i++){
+        for(int j=0;j<COLUMN;j++){
+            printf("%d\t", array[i][j]);
+        }
+        printf("\n");
+    }
+}
